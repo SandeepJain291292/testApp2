1d6f06c54d8c626390eca0a908a4db37631a504e
sandeep jain <sandeep.jain@fexle.com>
2021-03-16 16:36:15 +0530

    seesion unuse

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,16 @@ router.get('/', function(req, res, next) {
   req.session.city = 'appcity';
   res.render('index', { title: 'App2' });
   console.log('name--'+req.session);
+  console.log('id--'+req.sessionID);
   console.log('name 1--'+req.session.name);
   console.log('name city--'+req.session.city);
 });
 
 router.get('/path',function(req, res, next){
-  console.log('name--'+req.session);
-  console.log('name 1--'+req.session.name);
-  console.log('name city--'+req.session.city);
+  console.log('name-2-'+req.session);
+  console.log('id-2-'+req.sessionID);
+  console.log('name 1-2-'+req.session.name);
+  console.log('name city-2-'+req.session.city);
 });
 
 module.exports = router;
