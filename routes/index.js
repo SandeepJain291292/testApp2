b2e009653530ae90ea4f77ec9ce8f42230ea543e
sandeep jain <sandeep.jain@fexle.com>
2021-03-16 16:12:11 +0530

    seesion

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  req.session.name = 'app2';
   res.render('index', { title: 'App2' });
+  console.log('name--'+req.session.name);
 });
 
 module.exports = router;
